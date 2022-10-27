842bfb361a19096348eed7fb6aaf8adbf9e852ab
karkrish2009 <karkrish2009@gmail.com>
2022-10-27 13:57:13 -0700

    Turning In Assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
+import Stopwatch from "./components/timers/Stopwatch";
+import Countdown from "./components/timers/Countdown";
+import XY from "./components/timers/XY";
+import Tabata from "./components/timers/Tabata"
 
 import DocumentationView from "./views/DocumentationView";
 import TimersView from "./views/TimersView";
+import {useState, useEffect} from 'react';
 
 const Container = styled.div`
   background: #f0f6fb;
   height: 100vh;
   overflow: auto;
 `;
-
+/*
 const Nav = () => {
   return (
     <nav>
@@ -24,19 +30,58 @@ const Nav = () => {
       </ul>
     </nav>
   );
-};
-
+};*/
 const App = () => {
+
+  const [timer, setTimer] = useState("");
+
+  const timerSelect = event => {
+
+    if (event.target.value === "Stopwatch"){
+        setTimer("Stopwatch");
+        console.log(event.target.value);
+    }
+    else if (event.target.value === "Countdown"){
+        setTimer("Countdown");
+        console.log(event.target.value);
+    }
+    else if (event.target.value === "XY"){
+        setTimer("XY");
+        console.log(event.target.value);
+    }
+    else{
+      setTimer("Tabata");
+      console.log(event.target.value);
+    }
+  }
+
   return (
-    <Container>
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/docs" element={<DocumentationView />} />
-          <Route path="/" element={<TimersView />} />
-        </Routes>
-      </Router>
-    </Container>
+    <div>
+    <label for="timer-select">Choose a Timer:</label>
+
+    <select name="timer" id="timer" onChange={timerSelect}>
+      <option value="">--Please choose an option--</option>
+      <option value="Stopwatch">Stopwatch</option>
+      <option value="Countdown">Countdown</option>
+      <option value="XY">XY Timer</option>
+      <option value="Tabata">Tabata</option>
+    </select>
+    <div>
+    {timer === "Stopwatch" && (
+      <Stopwatch />
+    )}
+    {timer === "Countdown" && (
+      <Countdown />
+    )}
+    {timer === "XY" && (
+      <XY />
+    )}
+    {timer === "Tabata" && (
+      <Tabata />
+    )
+    }
+    </div>
+    </div>
   );
 };
 
